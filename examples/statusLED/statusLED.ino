4ebf762d85e08b191ef0e9a4f1c70b55f6ace888
TheDIYGuy999 <msommer74@gmail.com>
2016-06-11 08:17:08 +0200

    Create statusLED.ino

---
+++ examples/statusLED/statusLED.ino
@@ -0,0 +1,40 @@
+/*
+  statusLED.h - Library for status LED control (blinking, flash sequences, on, off, pwm)
+  Created by TheDIYGuy999 May 2016
+  Released into the public domain.
+*/
+
+#include <statusLED.h> // TheDIYGuy999 library: https://github.com/TheDIYGuy999/statusLED
+
+// Status LED objects
+statusLED greenLED;
+statusLED redLED;
+
+void setup() {
+  // put your setup code here, to run once:
+
+  // LED setup
+  greenLED.begin(5); // Green LED on pin 5
+  redLED.begin(6); // Red LED on pin 6
+
+// LED demo (1x executed)
+  redLED.on();
+  delay(2000);
+  greenLED.on();
+  delay(1000);
+  redLED.pwm(10); // PWM capable pins required!
+  greenLED.pwm(20);
+  delay(2000);
+  greenLED.off();
+  delay(1000);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  // Flashing sequences
+  redLED.flash(140, 150, 700, 5); // ON, OFF, PAUSE, PULSES
+  greenLED.flash(40, 350, 0, 0);
+
+
+}
