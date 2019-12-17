074d85310ec1ab47d741a48a46c85f0066a9f90f
TheDIYGuy999 <msommer74@gmail.com>
2019-12-17 16:19:09 +0100

    ESP32 support added

--- examples/statusLED/statusLED.ino
+++ examples/statusLED/statusLED.ino
@@ -13,18 +13,31 @@ statusLED redLED(false);
 void setup() {
   // put your setup code here, to run once:
 
+
   // LED setup
+
+  // AVR platform:
+#if defined __AVR_ATmega32U4__ || __AVR_ATmega328P__
   greenLED.begin(5); // Green LED on pin 5
   redLED.begin(6); // Red LED on pin 6
 
-// LED demo (1x executed)
+  // ESP32 platform: use a separate timer for each LED, but not timer 0, because of millis()
+#else
+  greenLED.begin(33, 2, 31500); // Green LED on pin 33, Timer 2, 31500Hz
+  redLED.begin(32, 3, 31500); // Green LED on pin 32, Timer 3, 31500Hz
+#endif
+
+  // LED demo (1x executed)
   redLED.on();
   delay(2000);
   greenLED.on();
   delay(1000);
   redLED.pwm(10); // PWM capable pins required!
-  greenLED.pwm(20);
-  delay(2000);
+  delay(1000);
+  greenLED.pwm(50);
+  delay(1000);
+  redLED.off();
+  delay(1000);
   greenLED.off();
   delay(1000);
 }
@@ -35,4 +48,6 @@ void loop() {
   // Flashing sequences
   redLED.flash(140, 150, 700, 5); // ON, OFF, PAUSE, PULSES
   greenLED.flash(40, 350, 0, 0);
+
+
 }
